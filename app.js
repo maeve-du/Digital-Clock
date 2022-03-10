37e3a022aa691f7293f637fc3212f5bb29cbb69e
maeve-du <98177760+maeve-du@users.noreply.github.com>
2022-03-10 18:12:52 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const clock = document.querySelector('.clock')
+
+
+const tick = () => {
+    const now = new Date();
+    // console.log(now);
+    const h = now.getHours();
+
+    let m = now.getMinutes();
+    let s = now.getSeconds()
+    // console.log('h', h, 'm', m, 's', s);
+    // console.log(clock);
+
+    let mHTML = ''
+    // if (m < 10) {
+    //     mHTML = `<span class="time">0${m}</span>`
+    // } else if (m === 0) {
+    //     mHTML = `<span class="time">00</span>`
+    // }
+    // else {
+    //     mHTML = `<span class="time">${m}</span>`
+    // };
+
+    m < 10 ? mHTML = `<span class="time">0${m}</span>`
+        : m === 0 ? mHTML = `<span class="time">00</span>`
+            : mHTML = `<span class="time">${m}</span>`
+
+    let sHTML = '';
+    if (s < 10) {
+        sHTML = `<span class="time">0${s}</span>`
+    } else if (s === 0) {
+        sHTML = `<span class="time">00</span>`
+    }
+    else {
+        sHTML = `<span class="time">${s}</span>`
+    };
+
+    clock.innerHTML = `
+    <span class="time"> ${h}</span><span class="flash"> : </span>
+    ${mHTML}<span class="flash"> : </span>
+    ${sHTML}
+    `
+}
+
+// tick()
+
+
+setInterval(() => {
+    tick()
+}, 1000)
